11a983f8b511a95c898d4f4c8152e379f129f03d
Lenin-Grub <dessotterro@mail.ru>
2024-08-01 22:08:38 +0300

    Add glew, spdlog and imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
-#include <iostream>
 #include "Window/Window.h"
 
 const unsigned int WIDTH  = 800;
@@ -12,11 +16,36 @@ int main()
     Window window;
     window.create(WIDTH, HEIGHT, "Test");
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window.m_window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
     while (!glfwWindowShouldClose(window.m_window))
     {
-        window.clear();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Test");
+        ImGui::Text("Hello world!");
+        ImGui::Button("Press me!");
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window.m_window);
+
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     window.close();
     return 0;
